cd42c2bee1807dba62e392f720afb49c2c791440
AmarAkhtayar <61712051+AmarAkhtayar@users.noreply.github.com>
2020-03-03 16:03:54 +0100

    Update Day_2_task_2_1.pl

--- Assignments/Logical/Day_2_Tasks/Day_2_task_2_1.pl
+++ Assignments/Logical/Day_2_Tasks/Day_2_task_2_1.pl
@@ -1,1 +1,47 @@
 
+/* a */
+
+all_as([ ]).
+all_as([H|T]):- H==a, all_as(T).
+
+/*** ?- all_as([a,a]).
+true. 
+
+?- all_as([a,b]).
+false.
+
+****/
+
+list_length([],0).
+list_length([_|T],N) :- list_length(T,X), N is X+1.
+
+
+
+
+
+replace([],[]). 
+replace([u|ITail],[i|OTail]):- replace(ITail,OTail),!.
+
+replace([i|ITail],[o|OTail]):- replace(ITail,OTail),!.
+
+replace([o|ITail],[u|OTail]):- replace(ITail,OTail),!.
+
+
+replace([Y|ITail],[Y|OTail]):- replace(ITail,OTail),!.
+
+/*** 
+?- replace([u,i,o],X).
+X = [i, o, u].
+
+?- replace([u,a,i],X).
+X = [i, a, o].
+
+ ?-  replace([u,a,a,o,i,i,i,i],X).
+X = [i, a, a, u, o, o, o, o].
+ 
+
+****/
+
+
+
+
